95ea8feb665c472d024c6f3fbf4d69a62a6838a9
ktrobert <rob21065@byui.edu>
2023-02-15 13:49:32 -0700

    discovery fixes

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,15 +1,11 @@
 const temperature = Number(document.getElementById("temperature").innerText);
 const windSpeed = Number(document.getElementById("windSpeed").innerText);
 
-console.log(temperature);
-console.log(windSpeed);
-
 if (temperature <= 50 && windSpeed > 3.0) {
     let windChill = 35.74 +
         0.6215 * temperature -
         35.75 * Math.pow(windSpeed, 0.16) +
         0.4275 * temperature * Math.pow(windSpeed, 0.16);
-
     document.getElementById("windChill").innerHTML = windChill.toFixed(2);
 } else {
     document.getElementById("windChill").innerHTML = "N/A";
