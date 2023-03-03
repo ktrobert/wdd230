21ac600bb1f994aad7dac37c7f8ad8b0d13f4394
ktrobert <rob21065@byui.edu>
2023-03-02 21:57:27 -0700

    directory test

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,9 +1,8 @@
-const url = "http://127.0.0.1:5555/chamber/directory.json";
+const url = "https://raw.githubusercontent.com/ktrobert/wdd230/main/chamber/directory.json";
 //the following code is for grid and list, not sure if I need to put before or after url.
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
-
+const display = document.querySelector("#businessgrid");
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
 gridbutton.addEventListener("click", () => {
@@ -22,33 +21,35 @@ function showList() {
 async function getDirectoryData(url) {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);
-    display(data.business);
+    console.log(data)
+        //console.table(data.prophets);
+    displayDirectory(data.business);
 }
 
-getProphetData(url);
+getDirectoryData(url);
 
-const displayDirectory = (business) => {
-    const cards = document.querySelector('div.cards'); // select the output container element
-
-    prophets.forEach(() => {
+const displayDirectory = (businesses) => {
+    const cards = display; //document.querySelector('.busi'); // select the output container element
+    businesses.forEach((business) => {
         // Create elements to add to the div.cards element
         let portrait = document.createElement('img');
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
-        let h3 = document.createElement('h3')
+        let h3 = document.createElement('h4')
         let p = document.createElement('p');
+        let a = document.createElement('a');
 
 
 
         // Build the h2 content out to show the prophet's full name - finish the template string
         h2.textContent = `${business.companyName}`;
         h3.textContent = `${business.phoneNumber}`;
-        p.textContent = `${prophet.address}`;
-        p.textContent = `${url}`
+        p.textContent = `${business.address}`;
+        a.textContent = `${business.url}`;
+        a.setAttribute('href', `${business.url}`)
             // Build the image portrait by setting all the relevant attribute
         portrait.setAttribute('src', business.imageurl);
-        portrait.setAttribute('alt', `${business.companyName} ${prophet.lastname}`);
+        portrait.setAttribute('alt', `${business.companyName}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '200');
         portrait.setAttribute('height', '200');
@@ -58,7 +59,7 @@ const displayDirectory = (business) => {
         card.appendChild(h2);
         card.appendChild(h3);
         card.appendChild(p);
-        card.appendChild(p);
+        card.appendChild(a);
 
         cards.appendChild(card);
     })
