39b24dc0e542d0906ab036d5af9532e49efe4c9f
ktrobert <rob21065@byui.edu>
2023-03-08 21:17:44 -0700

    weatherapi

---
+++ week10/weatherapi.js
@@ -0,0 +1,38 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const weatherLocation = 'Fairbanks';
+const weatherUnits = 'imperial';
+const weatherId = 'e2dff56ff0c0d4e0cad3ccbdd3065825';
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${weatherLocation}&units=${weatherUnits}&appid=${weatherId}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // this is for testing the call
+            // displayResults(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
