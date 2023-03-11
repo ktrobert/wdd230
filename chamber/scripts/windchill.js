fe5d7af6de83a4c8611dff437cfca67aaae2fc74
ktrobert <rob21065@byui.edu>
2023-03-10 17:05:41 -0700

    week10 commit

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,3 +1,4 @@
+/*
 const temperature = Number(document.getElementById("temperature").innerText);
 const windSpeed = Number(document.getElementById("windSpeed").innerText);
 
@@ -10,4 +11,43 @@ if (temperature <= 50 && windSpeed > 3.0) {
 } else {
     document.getElementById("windChill").innerHTML = "N/A";
 
+}
+*/
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const weatherLocation = 'American Fork';
+const weatherUnits = 'imperial';
+const weatherId = 'e2dff56ff0c0d4e0cad3ccbdd3065825';
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${weatherLocation}&units=${weatherUnits}&appid=${weatherId}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // this is for testing the call
+            // displayResults(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
 }
