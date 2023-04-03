69c67b20776ca66a161bb90eaad742cba3105ba6
ktrobert <rob21065@byui.edu>
2023-04-03 12:02:56 -0600

    brokens tuff

--- bountifulFoods/scripts/fresh.js
+++ bountifulFoods/scripts/fresh.js
@@ -1,9 +1,11 @@
 const url = "https://brotherblazzard.github.io/canvas-content/fruit.json";
+const urlimgs = "https://raw.githubusercontent.com/ktrobert/wdd230/main/bountifulFoods/fresh.json";
 
 const fruit1 = document.getElementById("fruit1");
 const fruit2 = document.getElementById("fruit2");
 const fruit3 = document.getElementById("fruit3");
 let fruitData;
+let fruitImgData;
 
 async function getFruitData(url) {
     const response = await fetch(url);
@@ -14,6 +16,12 @@ async function getFruitData(url) {
     createFruits(data,fruit2);
     createFruits(data,fruit3);
     
+}
+
+async function getFruitImageData(url) {
+    const response = await fetch(urlimgs);
+    const data = await response.json();
+    fruitImgData = data.fruit;
 }
 
 function createFruits(data, element){
@@ -62,6 +70,8 @@ function showFruitRow(fruit,table){
 
 
 
+
+
 function showRow(data,table){
     let row = document.createElement('tr');
     for(let i = 0; i < data.length; i++){
@@ -73,6 +83,33 @@ function showRow(data,table){
 }
 
 getFruitData(url);
+getFruitImageData(url);
+setupImageEvent();
+function setupImageEvent(){
+    fruit1.onchange = (evt) =>{
+        let img = document.getElementById("fruit1img");
+        let label = document.getElementById("fruit1label");
+        let newsrc = fruitImgData[fruit1.value].imageurl;
+        img.setAttribute("src",newsrc);
+        label.innerHTML = fruitImgData[fruit1.value].name;
+    }
+    fruit2.onchange = (evt) =>{
+        let img = document.getElementById("fruit2img");
+        let label = document.getElementById("fruit2label");
+        let newsrc = fruitImgData[fruit2.value].imageurl;
+        img.setAttribute("src",newsrc);
+        label.innerHTML = fruitImgData[fruit2.value].name;
+    }
+    fruit3.onchange = (evt) =>{
+        let img = document.getElementById("fruit3img");
+        let label = document.getElementById("fruit3label");
+        let newsrc = fruitImgData[fruit3.value].imageurl;
+        img.setAttribute("src",newsrc);
+        label.innerHTML = fruitImgData[fruit3.value].name;
+    }
+}
+
+
 
 
 function handleSubmit(fruit) {
